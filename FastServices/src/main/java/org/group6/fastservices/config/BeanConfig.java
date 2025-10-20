3255e554b97cb8c099f11233292d2da68e5460ef
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-20 10:13:45 +0100

    update bean config

--- FastServices/src/main/java/org/group6/fastservices/config/BeanConfig.java
+++ FastServices/src/main/java/org/group6/fastservices/config/BeanConfig.java
@@ -2,9 +2,11 @@ package org.group6.fastservices.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class BeanConfig {
 
     @Bean
