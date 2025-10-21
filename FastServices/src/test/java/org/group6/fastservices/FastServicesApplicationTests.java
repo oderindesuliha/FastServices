c9b1539a07abe8f1e8a902783393ed221e692732
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-21 13:03:44 +0100

    updated repositories

--- FastServices/src/test/java/org/group6/fastservices/FastServicesApplicationTests.java
+++ FastServices/src/test/java/org/group6/fastservices/FastServicesApplicationTests.java
@@ -1,13 +1,37 @@
 package org.group6.fastservices;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class FastServicesApplicationTests {
 
+    @Value("${TEST_DB_PASSWORD}")
+    private String password;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testCanConnectToDatabase() {
+        try (HikariDataSource dataSource = new HikariDataSource()) {
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/fastservice");
+            dataSource.setUsername("postgres");
+            dataSource.setPassword(password);
+
+            Connection connection = dataSource.getConnection();
+            assertNotNull(connection);
+        } catch(Exception e) {
+            assertNotNull(e);
+        }
+
     }
 
 }
--- FastServices/src/test/java/org/group6/fastservices/repository/UserRepositoryTest.java
+++ FastServices/src/test/java/org/group6/fastservices/repository/UserRepositoryTest.java
@@ -4,7 +4,6 @@ import org.group6.fastservices.data.models.User;
 import org.group6.fastservices.data.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -13,23 +12,22 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Sql(scripts = {"/db/data.sql"})
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    @Sql(scripts = {"/db/data.sql"})
     void testFindByEmail() {
-        Optional<User> user = userRepository.findByEmail("chinedu.okonkwo@gmail.com");
-        assertTrue(user.isPresent());
-        assertEquals("Chinedu", user.get().getFirstName());
-        assertEquals("Okonkwo", user.get().getLastName());
+        Optional<User> foundUser = userRepository.findByEmail("chinedu.okonkwo@gmail.com");
+        assertTrue(foundUser.isPresent());
+        assertEquals("Chinedu", foundUser.get().getFirstName());
+        assertEquals("Okonkwo", foundUser.get().getLastName());
     }
 
     @Test
-    @Sql(scripts = {"/db/data.sql"})
     void testExistsByEmail() {
         boolean exists = userRepository.existsByEmail("adunni.alao@gmail.com");
         assertTrue(exists);
