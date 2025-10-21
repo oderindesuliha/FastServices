daa50377437b80b70257b3642257d364b0e0a5c4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-21 13:47:31 +0100

    created customer and admin repository

--- FastServices/src/main/java/org/group6/fastservices/data/repositories/AdminRepository.java
+++ FastServices/src/main/java/org/group6/fastservices/data/repositories/AdminRepository.java
@@ -1,4 +1,9 @@
 package org.group6.fastservices.data.repositories;
 
-public interface AdminRepository {
+import org.group6.fastservices.data.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, String> {
 }
