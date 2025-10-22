fc6338b556bb8be9d03195586deb4a3befebc5a3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-22 20:11:01 +0100

    created admin serviceimpl

--- FastServices/src/main/java/org/group6/fastservices/services/impl/AdminServiceImpl.java
+++ FastServices/src/main/java/org/group6/fastservices/services/impl/AdminServiceImpl.java
@@ -1,12 +1,21 @@
 package org.group6.fastservices.services.impl;
 
+import lombok.AllArgsConstructor;
 import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
 import org.group6.fastservices.dtos.responses.RegisterOrgResponse;
 import org.group6.fastservices.services.AdminService;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AdminServiceImpl implements AdminService {
+
     @Override
     public RegisterOrgResponse registerOrg(RegisterOrgRequest register) {
-        return null;
+        return new RegisterOrgResponse();
+    }
+
+    private void verifyNewEmail(String email) {
+        if()
     }
 }
