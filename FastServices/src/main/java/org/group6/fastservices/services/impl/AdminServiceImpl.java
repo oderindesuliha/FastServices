0a24eb8084b5b4cb21dba1aab3db0ad7b8b78206
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-22 20:03:43 +0100

    created admin serviceimpl

---
+++ FastServices/src/main/java/org/group6/fastservices/services/impl/AdminServiceImpl.java
@@ -0,0 +1,12 @@
+package org.group6.fastservices.services.impl;
+
+import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
+import org.group6.fastservices.dtos.responses.RegisterOrgResponse;
+import org.group6.fastservices.services.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public RegisterOrgResponse registerOrg(RegisterOrgRequest register) {
+        return null;
+    }
+}
