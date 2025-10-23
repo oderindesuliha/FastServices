e11f82fdeadf9638b3a0794407ba8c1663a13ce2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-23 13:10:31 +0100

    register org moved from admin service to Org service

--- FastServices/src/main/java/org/group6/fastservices/services/AdminService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/AdminService.java
@@ -1,9 +1,5 @@
 package org.group6.fastservices.services;
 
-import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
-import org.group6.fastservices.dtos.responses.RegisterOrgResponse;
-
 public interface AdminService {
 
-    RegisterOrgResponse registerOrg(RegisterOrgRequest register);
 }
--- FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
@@ -1,15 +1,16 @@
 package org.group6.fastservices.services;
 
 import org.group6.fastservices.data.models.Organization;
+import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
 
 import java.util.List;
 
 public interface OrganizationService {
-    Organization createOrganization(Organization organization);
-    Organization getOrganizationById(String id);
-    Organization getOrganizationByCode(String code);
-    List<Organization> getAllOrganizations();
-    Organization updateOrganization(String id, Organization organization);
-    void deleteOrganization(String id);
-    boolean existsByCode(String code);
+    RegisterOrgRequest registerOrganization(RegisterOrgRequest registerRequest);
+//    Organization getOrganizationById(String id);
+//    Organization getOrganizationByCode(String code);
+//    List<Organization> getAllOrganizations();
+//    Organization updateOrganization(String id, Organization organization);
+//    void deleteOrganization(String id);
+//    boolean existsByCode(String code);
 }
