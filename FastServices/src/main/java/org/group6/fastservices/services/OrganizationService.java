71cd32a6964216088d8e718faefb4f3b465d0ade
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-23 17:18:25 +0100

    org can create service test

--- FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
@@ -1,5 +1,6 @@
 package org.group6.fastservices.services;
 
+import org.group6.fastservices.dtos.requests.CreateServiceRequest;
 import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
 import org.group6.fastservices.dtos.responses.CreateServiceResponse;
 import org.group6.fastservices.dtos.responses.RegisterOrgResponse;
