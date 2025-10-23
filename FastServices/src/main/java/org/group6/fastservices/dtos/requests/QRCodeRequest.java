52cbb94f028c096689b623474497ebeb99aa5ee7
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-23 13:42:54 +0100

    updated qrcode util

--- FastServices/src/main/java/org/group6/fastservices/dtos/requests/QRCodeRequest.java
+++ FastServices/src/main/java/org/group6/fastservices/dtos/requests/QRCodeRequest.java
@@ -1,12 +1,67 @@
 package org.group6.fastservices.dtos.requests;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@RequiredArgsConstructor
 public class QRCodeRequest {
     private String text;
-    private int width = 300;   // default size
-    private int height = 300;  // default size
+    private int width = 300;
+    private int height = 300;
 }
+
+package org.group6.fastservices.utils;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.group6.fastservices.dtos.requests.QRCodeRequest;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+        import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeGenerator {
+
+    public static String generateQRCode(QRCodeRequest request) throws WriterException, IOException {
+        String text = request.getText();
+        int width = request.getWidth();
+        int height = request.getHeight();
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, width, height);
+        graphics.setColor(Color.BLACK);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (bitMatrix.get(x, y)) {
+                    graphics.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+    }
+}
+
--- FastServices/src/main/java/org/group6/fastservices/utils/QRCodeGenerator.java
+++ FastServices/src/main/java/org/group6/fastservices/utils/QRCodeGenerator.java
@@ -6,6 +6,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.group6.fastservices.dtos.requests.QRCodeRequest;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -18,36 +19,30 @@ import java.util.Map;
 
 public class QRCodeGenerator {
     
-    public static String generateQRCode(String text, int width, int height) throws WriterException, IOException {
-        // Create a QR code writer
+    public static String generateQRCode(QRCodeRequest request) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         
-        // Set encoding hints
         Map<EncodeHintType, Object> hints = new HashMap<>();
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
         
-        // Generate the QR code matrix
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
-        
-        // Convert to BufferedImage
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        BitMatrix bitMatrix = qrCodeWriter.encode(request.getText(), BarcodeFormat.QR_CODE, request.getWidth(), request.getHeight(), hints);
+        BufferedImage image = new BufferedImage(request.getWidth(), request.getHeight(), BufferedImage.TYPE_INT_RGB);
         image.createGraphics();
         
         Graphics2D graphics = (Graphics2D) image.getGraphics();
         graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, width, height);
+        graphics.fillRect(0, 0, request.getWidth(), request.getHeight());
         graphics.setColor(Color.BLACK);
         
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (bitMatrix.get(i, j)) {
-                    graphics.fillRect(i, j, 1, 1);
+        for (int count = 0; count < request.getWidth(); count++) {
+            for (int counter = 0; counter < request.getHeight(); counter++) {
+                if (bitMatrix.get(count, counter)) {
+                    graphics.fillRect(count, counter, 1, 1);
                 }
             }
         }
-        
-        // Convert to Base64 string
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "png", baos);
         byte[] bytes = baos.toByteArray();
