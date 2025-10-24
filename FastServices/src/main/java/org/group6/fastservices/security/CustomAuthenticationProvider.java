59c597fc4bf484d547ea557603527b8d1b0c8878
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-24 10:40:53 +0100

    creatde custom auth rpovider

--- FastServices/src/main/java/org/group6/fastservices/security/CustomAuthenticationProvider.java
+++ FastServices/src/main/java/org/group6/fastservices/security/CustomAuthenticationProvider.java
@@ -1,9 +1,15 @@
 package org.group6.fastservices.security;
 
 import lombok.AllArgsConstructor;
+import org.group6.fastservices.data.models.Role;
+import org.group6.fastservices.exceptions.InvalidRoleException;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -19,13 +25,23 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String usernameOrEmailorCode = authentication.getName();
         String password = authentication.getCredentials().toString();
+        String roleFromReq = authentication.getDetails().toString();
 
+        Role role = parseUserRole(roleFromReq);
+        UserDetailsService userService = serviceResolver.getServiceForRole(role);
+        UserDetails userDetails = userService.loadUserByUsername(usernameOrEmailorCode);
 
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Incorrect password");
+        }
+        return new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities()
+        );
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 
     private Role parseUserRole(String role) {
--- FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/OrganizationService.java
@@ -5,7 +5,6 @@ import org.group6.fastservices.dtos.requests.RegisterOrgRequest;
 import org.group6.fastservices.dtos.responses.CreateServiceResponse;
 import org.group6.fastservices.dtos.responses.RegisterOrgResponse;
 
-
 public interface OrganizationService {
     RegisterOrgResponse registerOrganization(RegisterOrgRequest registerRequest);
     CreateServiceResponse createService(CreateServiceRequest request);
