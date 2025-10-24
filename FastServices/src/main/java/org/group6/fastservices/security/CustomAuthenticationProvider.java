19756101f6f9f57af46337a1e70d4810a5123af4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-24 07:05:46 +0100

    working on custom auth provider

--- FastServices/src/main/java/org/group6/fastservices/security/CustomAuthenticationProvider.java
+++ FastServices/src/main/java/org/group6/fastservices/security/CustomAuthenticationProvider.java
@@ -1,12 +1,18 @@
 package org.group6.fastservices.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final CustomServiceResolver serviceResolver;
+    private final PasswordEncoder passwordEncoder;
+
 
 
     @Override
