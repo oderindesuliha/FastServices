382b36a8bf5f58b073a6370f0eecd647d6b30de5
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-27 04:18:30 +0100

    update

--- FastServices/src/main/java/org/group6/fastservices/dtos/requests/QueueRequest.java
+++ FastServices/src/main/java/org/group6/fastservices/dtos/requests/QueueRequest.java
@@ -1,8 +1,11 @@
 package org.group6.fastservices.dtos.requests;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 public class QueueRequest {
     private String name;
     private String description;
