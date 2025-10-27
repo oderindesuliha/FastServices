006e6b1a8e3c5ec256ab99468fa100a3ea77aacf
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-27 04:24:32 +0100

    update

--- FastServices/src/main/java/org/group6/fastservices/dtos/responses/AppointmentResponse.java
+++ FastServices/src/main/java/org/group6/fastservices/dtos/responses/AppointmentResponse.java
@@ -1,14 +1,14 @@
 package org.group6.fastservices.dtos.responses;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.group6.fastservices.data.models.AppointmentStatus;
 
 import java.time.LocalDateTime;
 
-@Data
+@Setter
+@Getter
 public class AppointmentResponse {
-    private String id;
-    private String userId;
     private String offeringId;
     private String queueId;
     private LocalDateTime appointmentDate;
