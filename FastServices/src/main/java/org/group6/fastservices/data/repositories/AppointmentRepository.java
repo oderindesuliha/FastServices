b017e7ec52cdaf2b1191595453c8b15b111f339c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-27 04:17:37 +0100

    update

--- FastServices/src/main/java/org/group6/fastservices/data/repositories/AppointmentRepository.java
+++ FastServices/src/main/java/org/group6/fastservices/data/repositories/AppointmentRepository.java
@@ -4,7 +4,6 @@ import org.group6.fastservices.data.models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.List;
 
 @Repository
