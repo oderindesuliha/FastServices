dd0da140693b659e57abfed8263c9bf66187687b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-29 06:27:40 +0100

    create appointment service test

--- FastServices/src/test/java/org/group6/fastservices/services/impl/AppointmentServiceImplTest.java
+++ FastServices/src/test/java/org/group6/fastservices/services/impl/AppointmentServiceImplTest.java
@@ -2,14 +2,20 @@ package org.group6.fastservices.services.impl;
 
 import org.group6.fastservices.data.models.Appointment;
 import org.group6.fastservices.data.models.AppointmentStatus;
+import org.group6.fastservices.data.models.Organization;
 import org.group6.fastservices.data.repositories.AppointmentRepository;
 import org.group6.fastservices.dtos.requests.CreateAppointmentRequest;
+import org.group6.fastservices.dtos.requests.RegisterUserRequest;
 import org.group6.fastservices.dtos.responses.CreateAppointmentResponse;
+import org.group6.fastservices.dtos.responses.RegisterUserResponse;
+import org.group6.fastservices.security.AuthenticatedPrincipal;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
 
@@ -22,6 +28,7 @@ class AppointmentServiceImplTest {
     private AppointmentServiceImpl appointmentService;
     @Autowired
     private AppointmentRepository appointmentRepository;
+    @Autowired
 
     @BeforeEach
     void setUp() {
@@ -46,6 +53,24 @@ class AppointmentServiceImplTest {
         request.setStatus(AppointmentStatus.PENDING);
 
         return appointmentService.createAppointment(request);
+    }
+
+    private void mockCustomerAuthentication() {
+        Organization org = new Organization();
+        org.setName("YabaTech");
+        org.setCode("YAB001");
+        org.setContactEmail("org@mail.com");
+        org.setContactPhone("08012345678");
+        org.setPassword("password");
+        org.setRole("ORGANIZATION");
+
+        var savedOrg = organizationRepository.save(org);
+        AuthenticatedPrincipal principal = new AuthenticatedPrincipal(savedOrg);
+
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(
+                        principal, null, principal.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
 }
