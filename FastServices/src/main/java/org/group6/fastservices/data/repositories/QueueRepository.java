1028b2f858bf27b1a77c90e1698a132d901f8487
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-30 19:28:21 +0100

    working on  appointment service test

--- FastServices/src/main/java/org/group6/fastservices/data/repositories/QueueRepository.java
+++ FastServices/src/main/java/org/group6/fastservices/data/repositories/QueueRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface QueueRepository extends JpaRepository<Queue, String> {
     List<Queue> findByOrganizationId(String organizationId);
     List<Queue> findByAppointmentsOfferingId(String offeringId);
+
+    Optional<Queue> findByOfferingName(String offeringName);
 }
