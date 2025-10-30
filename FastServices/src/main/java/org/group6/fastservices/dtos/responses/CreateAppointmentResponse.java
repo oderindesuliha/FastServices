ea3db068c076d5d3f971fdcfc2c37617df77b377
BramTechxxvi <bramtechxxvi@gmail.com>
2025-10-30 16:37:15 +0100

    created exception class service not found

--- FastServices/src/main/java/org/group6/fastservices/dtos/responses/CreateAppointmentResponse.java
+++ FastServices/src/main/java/org/group6/fastservices/dtos/responses/CreateAppointmentResponse.java
@@ -1,5 +1,6 @@
 package org.group6.fastservices.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.group6.fastservices.data.models.AppointmentStatus;
@@ -8,15 +9,12 @@ import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class CreateAppointmentResponse {
-    private String userId;
-    private String offeringId;
-    private String queueId;
     private LocalDateTime appointmentDate;
     private AppointmentStatus status;
     private int queuePosition;
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
     private String message;
     private boolean success;
 }
