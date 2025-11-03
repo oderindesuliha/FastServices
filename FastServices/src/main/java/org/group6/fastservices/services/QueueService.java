546f2c3687bfa248fc6b23e29a7ad5d101b57377
BramTechxxvi <bramtechxxvi@gmail.com>
2025-11-03 13:27:01 +0100

    created queue dtos

--- FastServices/src/main/java/org/group6/fastservices/services/QueueService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/QueueService.java
@@ -1,18 +1,20 @@
 package org.group6.fastservices.services;
 
 import org.group6.fastservices.data.models.Queue;
+import org.group6.fastservices.dtos.requests.CreateQueueRequest;
+import org.group6.fastservices.dtos.responses.CreateQueueResponse;
 
 import java.util.List;
 
 public interface QueueService {
-    CreateQueueResponse createQueue(CreateQueueRequest request);
-    Queue getQueueById(String id);
-    List<Queue> getQueuesByOrganizationId(String organizationId);
-    List<Queue> getQueuesByOfferingId(String offeringId);
-    List<Queue> getAllQueues();
-    Queue updateQueue(String id, Queue queue);
-    void deleteQueue(String id);
-}
+    CreateQueueResponse findOrCreateQueueForOffering(CreateQueueRequest request);
+//    Queue getQueueById(String id);
+//    List<Queue> getQueuesByOrganizationId(String organizationId);
+//    List<Queue> getQueuesByOfferingId(String offeringId);
+//    List<Queue> getAllQueues();
+//    Queue updateQueue(String id, Queue queue);
+//    void deleteQueue(String id);
+//}
 
 
 
