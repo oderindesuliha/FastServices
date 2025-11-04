0cef3afae62832e2bab32eb8a9f1c9ac8ebda074
BramTechxxvi <bramtechxxvi@gmail.com>
2025-11-04 16:29:40 +0100

    created organization exception

--- FastServices/src/main/java/org/group6/fastservices/security/CustomOrganizationDetailsService.java
+++ FastServices/src/main/java/org/group6/fastservices/security/CustomOrganizationDetailsService.java
@@ -18,5 +18,4 @@ public class CustomOrganizationDetailsService implements UserDetailsService {
                 .map(AuthenticatedPrincipal::new)
                 .orElseThrow(() -> new UsernameNotFoundException("Organization not found"));
     }
-
 }
