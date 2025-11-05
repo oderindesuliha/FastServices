4db96e479f021799efd46c3614501f9b4781f4f4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-11-05 14:01:47 +0100

    craeted appointmentresponse dto

--- FastServices/src/main/java/org/group6/fastservices/services/EmailService.java
+++ FastServices/src/main/java/org/group6/fastservices/services/EmailService.java
@@ -5,16 +5,3 @@ public interface EmailService {
     void sendAppointmentConfirmation(String to, String appointmentDetails);
     void sendQueueNotification(String to, String queueDetails);
 }
-
-public class AppointmentResponse {
-    private String id;
-    private String offeringName;
-    private String organizationName;
-    private String customerName;
-    private LocalDateTime appointmentDate;
-    private String status;
-    private Integer queuePosition;
-    private LocalDateTime createdAt;
-
-}
-
