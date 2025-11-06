0c0749e25d74becf01d17ab6fa3c27b85fbc833c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-11-06 10:54:10 +0100

    updating appointment service

--- FastServices/src/main/java/org/group6/fastservices/services/impl/AppointmentServiceImpl.java
+++ FastServices/src/main/java/org/group6/fastservices/services/impl/AppointmentServiceImpl.java
@@ -56,7 +56,7 @@ public class AppointmentServiceImpl implements AppointmentService {
         appointment.setCreatedAt(LocalDateTime.now());
         appointment.setQueuePosition(nextPosition);
 
-        AppointmentResponse savedAppointment = appointmentRepository.save(appointment);
+        Appointment savedAppointment = appointmentRepository.save(appointment);
 
         return CreateAppointmentResponse.builder()
                 .id(savedAppointment.getId())
@@ -125,7 +125,7 @@ public class AppointmentServiceImpl implements AppointmentService {
                 .orElseThrow(()-> new AccountNotFoundException("Authenticated customer not found"));
     }
 
-    private AppointmentResponse mapToResponse(AppointmentResponse appointment) {
+    private AppointmentResponse mapToResponse(Appointment appointment) {
         return AppointmentResponse.builder()
                 .id(appointment.getId())
                 .offeringName(appointment.getOffering().getName())
